c89a57b5510fbb35eb4aa47cc4f2c8761177d353
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-11 10:30:33 +0900

    padStart를 이용해 시간의 한자리 숫자를 두자리로 변환

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minute = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerHTML = `${hours}:${minute}:${seconds}`;
 }
 
 getClock();
