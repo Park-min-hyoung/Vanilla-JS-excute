0ed604bc0d22ef22bbf4dc7346fcf3f3f8a25410
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-11 11:06:34 +0900

    ToDoList 준비

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 const choseImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = document.createElement("img"); // html element 생성(html에 직접 작성하는 것이 아니라 js가 만들어 주는 것)
 
 bgImage.src = `img/${choseImage}`;
 
