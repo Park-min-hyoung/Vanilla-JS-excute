ec29141610059908c9e64a1072045ddf514e2c4a
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-22 08:08:14 +0900

    element 부르기, 이벤트 기다리고 함수 실행

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const imgs = ["1.jpg", "2.jpg", "3.jpg"];
-
-const choseImage = imgs[Math.floor(Math.random() * imgs.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${choseImage}`;
-
-document.body.appendChild(bgImage);
