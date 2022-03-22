526b9c37a5b90a17fd717a53421e8a378ee08a86
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-22 17:17:51 +0900

    toDoList 항목 추가

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,20 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo) {
+  const toDoli = document.createElement("li");
+  const toDospan = document.createElement("span");
+  toDospan.innerText = newToDo;
+
+  toDoli.appendChild(toDospan);
+  toDoList.appendChild(toDoli);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
-  console.log(newTodo);
+  toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
