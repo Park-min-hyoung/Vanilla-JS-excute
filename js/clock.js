b151826917f1027c1bfb9e96f864234d26bd0054
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-22 16:51:23 +0900

    setInterval 및 Date를 이용해 시간 출력

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("hi");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 1000);
+getClock();
+setInterval(getClock, 1000);
