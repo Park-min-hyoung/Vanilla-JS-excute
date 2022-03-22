c23b4a6541eacca99f31daa82e104be9bd4ba632
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-22 17:30:05 +0900

    JSON.stringify를 이용해 localStorage에 문자열로 저장, toDoList 항목 저장 기능 준비

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const toDos = [];
+
+function localSaveToDos() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const deleteLi = event.target.parentElement;
   deleteLi.remove();
@@ -24,6 +30,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+
+  toDos.push(newTodo);
+  localSaveToDos();
   paintToDo(newTodo);
 }
 
