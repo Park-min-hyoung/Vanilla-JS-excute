fb39129673b96d13e8445ed65673a761e04edc93
Park-min-hyoung <qkralswgud1324@naver.com>
2022-03-28 15:35:29 +0900

    JSON.parse 및 forEach를 이용해 배열 생성 및 item 사용

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "toDos";
+
 const toDos = [];
 
 function localSaveToDos() {
-  localStorage.setItem("toDos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
@@ -37,3 +39,10 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// program 시작, localstorage에 item 여부 확인
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach((item) => console.log(item));
+}
